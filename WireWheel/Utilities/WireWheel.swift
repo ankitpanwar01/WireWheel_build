74a1d4ec01a68d8c035335e5b972d3a00b1988f3
Ankit Panwar <ankit.panwarmca@gmail.com>
2023-08-09 17:20:11 +0530

    version 0.0.8

--- WireWheel/Utilities/WireWheel.swift
+++ WireWheel/Utilities/WireWheel.swift
@@ -1,23 +1,19 @@
 //
-//  ConnectorViewController.swift
+//  WireWheel.swift
 //  WireWheel
 //
 //  Created by Ankit Panwar on 09/08/23.
 //
 
+import Foundation
 import UIKit
 
-public class ConnectorViewController: UIViewController {
 
-    public override func viewDidLoad() {
-        super.viewDidLoad()
-
-    }
+public struct WireWheel {
 
     public static func configure(view: UIViewController) {
-        let storyboard = UIStoryboard.init(name: "Form", bundle: Bundle.main)
+        let storyboard = UIStoryboard.init(name: "Form", bundle: .main)
         let viewContorller = storyboard.instantiateViewController(withIdentifier: "ListViewController") as! ListViewController
         view.present(viewContorller, animated: true)
     }
-
 }
